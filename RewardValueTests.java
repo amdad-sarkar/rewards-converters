f75985853f5b9861a07302f3a3831243151cb744
amdad-sarkar <88956209+amdad-sarkar@users.noreply.github.com>
2023-12-06 15:02:47 +0530

    Add files via upload
    task 1 all test cases are pass

---
+++ RewardValueTests.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RewardValueTests {
+
+    @Test
+    void create_with_cash_value() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
+    }
+
+    @Test
+    void create_with_miles_value() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
+    }
+
+    @Test
+    void convert_from_cash_to_miles() {
+        double cash = 127.98;
+        int expectedMiles = 36566;
+        RewardValue rewardValue = new RewardValue(cash);
+        assertEquals(expectedMiles, rewardValue.getMilesValue());
+    }
+
+    @Test
+    void convert_from_miles_to_cash() {
+        int miles = 36566;
+        double expectedCash = 127.98;
+        RewardValue rewardValue = new RewardValue(miles);
+        assertEquals(expectedCash, rewardValue.getCashValue());
+    }
+}
